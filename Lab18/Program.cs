3e4062fd7737717121e8bdf3e7279622e7bb8c5d
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-23 12:52:53 +0300

    Добавьте файлы проекта.

---
+++ Lab18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку со скобками");
+            string str = Console.ReadLine();
+            //Console.WriteLine(Check(str));
+            if (Check(str))
+                Console.WriteLine("Все хорошо");
+            else
+                Console.WriteLine("Неверная расстановка скобок");
+            Console.ReadKey();
+        }
+
+        static bool Check(string str)
+        { 
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> dic = new Dictionary<char, char>
+            {
+                {'(',')' },
+                {'{','}' },
+                {'[',']' }, };
+
+            foreach(char c in str)
+            {
+                if (c == '(' || c =='[' || c =='{')
+                    stack.Push(dic[c]);
+                if (c== ')' || c =='}' || c ==']')
+                {
+                    if (stack.Count==0 || stack.Pop()!=c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
